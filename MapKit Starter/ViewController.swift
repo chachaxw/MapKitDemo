9f0611ebc4c3c687676793a7e3123084be440bab
Wei Zhou <867571123@qq.com>
2017-01-18 14:33:31 +0800

    init a project

---
+++ MapKit Starter/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MapKit Starter
+//
+//  Created by Pranjal Satija on 10/25/16.
+//  Copyright © 2016 Pranjal Satija. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+    @IBOutlet var mapView: MKMapView?
+    
+    let locationManager = CLLocationManager()
+    
+    
+    override func viewDidLoad() {
+    }
+    
+}
+
